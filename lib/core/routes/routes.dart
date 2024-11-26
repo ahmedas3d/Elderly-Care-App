64b3d9ec3fbb8e89d9a74cfada3e7f6c6be0ce62
deadcodee1 <anaahmedasaad.4@gmail.com>
2024-11-26 11:38:32 +0200

    first commit

---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,22 @@
+import 'package:care_app/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const String homePage = '/';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case homePage:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            appBar: AppBar(),
+            body: const Center(
+              child: Text('Page not found'),
+            ),
+          ),
+        );
+    }
+  }
+}
---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainColor = Color(0xFF1648CE);
+  static const Color textColor = Color(0xFF6A788E);
+}
